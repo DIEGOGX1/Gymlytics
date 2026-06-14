4078676da8cc7441f9a41779f45bf3dbfd296f8d
Diego Gaxiola Olvera <gaxioladiego12@gmail.com>
2026-06-14 13:01:33 -0600

    Cambio de modelo a gemini-1.0-pro

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # Configuración del Cerebro de IA
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-modelo_ia = genai.GenerativeModel('gemini-1.5-flash') # Un modelo súper rápido y ligero
+modelo_ia = genai.GenerativeModel('gemini-1.0-pro') # Un modelo súper rápido y ligero
 
 # ==========================================
 # 1. CONFIGURACIÓN DE LA BASE DE DATOS
